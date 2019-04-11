518a034ac11401524525ec321849fc7206901b4f
Junichi Kawahara <kawahara-j@hi-ad.jp>
2019-04-11 18:41:24 +0900

    First Commit

---
+++ nippo.py
@@ -0,0 +1,26 @@
+#! /usr/local/bin/python3
+
+import openpyxl
+
+"""
+nippo.py
+日報ファイルの準備スクリプト
+Copyright (C) 2019 J.Kawahara
+2019.4.11 J.Kawahara 新規作成
+"""
+
+
+def prepare_nippo(filename):
+    """
+    日報ファイルの準備を行う
+
+    Parameters
+    ----------
+    filename : str
+        処理対象のファイル名
+    """
+    pass
+
+
+if __name__ == '__main__':
+    prepare_nippo('test.xlsx')

de22dd498a8402dcc821fdb1c856252497c98bd8
Junichi Kawahara <kawahara-j@hi-ad.jp>
2019-04-12 09:39:33 +0900

    仕上げた！

--- nippo.py
+++ nippo.py
@@ -1,6 +1,7 @@
 #! /usr/local/bin/python3
 
 import openpyxl
+import datetime
 
 """
 nippo.py
@@ -19,7 +20,21 @@ def prepare_nippo(filename):
     filename : str
         処理対象のファイル名
     """
-    pass
+    nowdt = datetime.date.today()
+    wb = openpyxl.load_workbook(filename)
+    if wb is None:
+        return
+    # ワークシートをコピーする
+    todays_sheet = wb.copy_worksheet(wb['雛形'])
+    todays_sheet.title = nowdt.strftime('%Y-%m-%d')
+    wb._sheets.sort(key=lambda ws: ws.title, reverse=True)
+
+    # 日付を入力する
+    serial_value = nowdt - datetime.date(1900, 1, 1)
+    todays_sheet['H1'].value = serial_value.days
+
+    # 保存する
+    wb.save(filename)
 
 
 if __name__ == '__main__':
